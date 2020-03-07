7b75214544e664b7dd175759f847bd95d7d52637
sebastian <sverdu6@gmail.com>
2020-03-07 02:51:36 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -8,23 +8,21 @@ function callMe() {
 }
 
 var crazy = "function";
-function crazy() {
   // fix the code in here:
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
-thisIsCrazy();
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
-
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
+  sayMyName();
 }
